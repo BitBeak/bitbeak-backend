ae4267ba5977accf05f6539ce91a6f9a8ac92791
Marcos <sbmarcos777@gmail.com>
2024-08-16 19:14:30 -0300

    ok

--- obj/Debug/net8.0/BitBeakAPI.AssemblyInfo.cs
+++ obj/Debug/net8.0/BitBeakAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BitBeakAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d89b2d4f298957d3a21abf741cd4ee1843e67866")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5dc28898df54109c88f4c6e333138295ec68f413")]
 [assembly: System.Reflection.AssemblyProductAttribute("BitBeakAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BitBeakAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
